90bae7e3a716e41037cd74a665be7e1563eadbb6
MewShairos <mewshairoz@gmail.com>
2023-02-06 17:24:35 -0300

    Data Fetching

--- src/App.js
+++ src/App.js
@@ -3,61 +3,52 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    counter: 0,
-    posts: [
-      {
-        id: 1,
-        title: 'O título 1',
-        body: 'O corpo 1'
-      },
-      {
-        id: 2,
-        title: 'O título 2',
-        body: 'O corpo 2'
-      },
-      {
-        id: 3,
-        title: 'O título 3',
-        body: 'O corpo 3'
-      }
-    ]
+    posts: []
   };
 
-  TimeoutUpdate = null;
-
   componentDidMount() {
-    this.handleTimeout();
+    this.loadPosts();
   }
 
   componentDidUpdate() {
-    this.handleTimeout();
   }
 
   componentWillUnmount() {
-    clearTimeout(this.TimeoutUpdate)
   }
 
-  handleTimeout = () => {
-    const { posts, counter } = this.state
-    posts[0].title = "Título Alterado"
+  loadPosts = async () => {
+    const postResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
 
-    this.TimeoutUpdate = setTimeout(() => {
-      this.setState({ posts, counter: counter + 1 });
-    }, 500);
+    const [posts, photos] = await Promise.all([postResponse, photosResponse]);
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos });
   }
+
+
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
     return (
-      <div className="App">
-        {counter}
-        {posts.map(post => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
